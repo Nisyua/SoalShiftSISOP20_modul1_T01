d745ac4b3f90891acf2e24dc679049093580dd3e
anissaidatur <50788092+anissaidatur@users.noreply.github.com>
2020-02-21 15:17:51 +0700

    Revisi

---
+++ Soal.1/soal1a.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "---Region dengan profit terkecil---"
+awk -F "\t" 'NR > 1 {arr[$13] =arr[$13]+=$NF}; END{for(key in arr)print key,arr[key]}' Sample-Superstore.tsv | sort -g -k 2 | head -1
+echo ""
+echo "---State dengan profit terkecil----"
+awk -F "\t" 'NR > 1 {if($13~"Central")arr[$11]+=$NF}; END{for(key in arr)print key,arr[key]}' Sample-Superstore.tsv | sort -nk 2 | head -2
+echo ""
+echo "---10 Product name yang memiliki profit paling sedikit dari 2 State.. ---" 
+awk -F "\t" 'NR>1{if($11~"Texas"||$11~"Illinois")arr[$17]+=$NF}END{for(key in arr)print arr[key],key}' Sample-Superstore.tsv | sort -g | head -n 10
+
